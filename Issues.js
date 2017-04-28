62452bc66b17c53603a5dae25a61c8b833882fc7
Stela Palii <scientia.ssp@gmail.com>
2017-04-28 10:05:55 -0700

    Six tests passing

--- Issues.js
+++ Issues.js
@@ -11,31 +11,48 @@ class Issues {
 
   // return an array containing each issues `id`
   get ids() {
+    return this.issues.map(issue => issue.id);
   }
 
   // return the total number of issues
   get numberOfIssues() {
+    return issues.length;
   }
 
   // return an array containing each issues `title`
   get titles() {
+    return this.issues.map(issue => issue.title);
   }
 
   // return an array containing the `login` property of the
   // `user` property for each issue. The array should not contain
   // any duplicate elements
   get users() {
+    let loginIds = {};
+    let users = this.issues.map(issue => issue.user.login);
+    users.forEach(function(elem) {
+      loginIds[elem] = 1;
+    });
+    return Object.keys(loginIds);
   }
 
   // return an array containing the `avatar_url` property of the
   // `user` property for each issue. The array should not contain
   // any duplicate elements
   get avatars() {
+    let avatars = {};
+    let users = this.issues.map(issue => issue.user.avatar_url);
+    users.forEach(function(elem) {
+      avatars[elem] = 1;
+    });
+    return Object.keys(avatars);
   }
 
   // return an array containing all the `id` property of the `assignedIssue`
   // property for each issue with an `assignee` property that is not `null`
   get withAssignee() {
+    let assign = this.issues.filter(issue => !issue.assignee);
+    console.log(assign);
   }
 
   // return an array containing all the `id` property of the `assignedIssue`
